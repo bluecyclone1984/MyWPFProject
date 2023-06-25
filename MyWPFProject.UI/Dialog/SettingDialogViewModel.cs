4ced691c36ee3451367d57f6e28f83719f2f548e
zhouyidong <ydzhou@i-con.cn>
2023-06-25 17:49:43 +0800

    添加设置对话框

---
+++ MyWPFProject.UI/Dialog/SettingDialogViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace MyWPFProject.UI
+{
+    public class SettingDialogViewModel: ViewModelBase
+    {
+        public SettingDialogViewModel()
+        {
+        }
+
+        private void initParams()
+        {
+            SelectedItemIndex = 0;
+        }
+
+        private int _selectedItemIndex;
+        public int SelectedItemIndex
+        {
+            get => _selectedItemIndex;
+            set => Set(ref _selectedItemIndex, value);
+        }
+
+        private ObservableCollection<TabItemModel> _tabitemList;
+        public ObservableCollection<TabItemModel> TabItemList
+        {
+            get => _tabitemList;
+            set => Set(ref _tabitemList, value);
+        }
+    }
+}
---
+++ MyWPFProject.UI/Model/TabItemModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyWPFProject.UI
+{
+    public class TabItemModel : ViewModelBase
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        private UserControl _content;
+        public UserControl Content
+        {
+            get => _content;
+            set => Set(ref _content, value);
+        }
+
+        private int _size = 16;
+        public int Size
+        {
+            get => _size;
+            set => Set(ref _size, value);
+        }
+    }
+}
