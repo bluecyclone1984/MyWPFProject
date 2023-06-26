edc2f561ac27ec4420852dd51157f04498c5ffaf
zhouyidong <ydzhou@i-con.cn>
2023-06-26 18:02:50 +0800

    简化实现 INotifyPropertyChanged

---
+++ MyWPFProject.UI/Model/NotifyPropertyChangedBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MyWPFProject.UI
+{
+    public abstract class NotifyPropertyChangedBase: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MyWPFProject.UI/Model/NotifyPropertyChangedInstance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using GalaSoft.MvvmLight;
+
+namespace MyWPFProject.UI
+{
+    public class NotifyPropertyChangedInstance : NotifyPropertyChangedBase
+    {
+        private string name;
+        private string code;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; OnPropertyChanged("Code"); }
+        }
+    }
+}
